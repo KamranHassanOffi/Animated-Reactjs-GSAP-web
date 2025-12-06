096d6bb7e691719cfc494d696b03c4fbc2701125
kamran <kamranhassan20044@gmail.com>
2025-12-06 21:01:27 +0500

    few addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
 import Home from './pages/home'
-import Agence from './pages/agence'
+import Agence from './pages/Agence'
 import Projects from './pages/projects'
 
 
 const App = () => {
   return (
     <div className='text-white'>
-   
+
 
       <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='agence' element={<Agence/>}/>
-      <Route path='/projects' element={<Projects/>}/>
-    </Routes> 
+        <Route path='/' element={<Home />} />
+        <Route path='/agence' element={<Agence />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
     </div>
   )
 }
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,8 +1,69 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
 import React from 'react'
-
+import { useRef } from 'react'
+gsap.registerPlugin(ScrollTrigger)
 const Agence = () => {
+  const imageDivRef = useRef(null)
+  const imageRef = useRef(null)
+  const imagesArray = [
+    'https://k72.ca/images/teamMembers/Olivier_480x640.jpg?w=480&h=640&fit=crop&s=c13569c0753117d04f1a93cf7b446d64',
+    'https://k72.ca/images/teamMembers/Lawrence_480x640.jpg?w=480&h=640&fit=crop&s=0a878205586092164001a9afe0ef4007',
+    'https://k72.ca/images/teamMembers/HugoJoseph_480x640.jpg?w=480&h=640&fit=crop&s=f152025b8a59b062d1e7978b5d6544c3',
+    'https://k72.ca/images/teamMembers/ChantalG_480x640.jpg?w=480&h=640&fit=crop&s=13093769c4a19cecd291ddcccd898991',
+    'https://k72.ca/images/teamMembers/SophieA_480x640.jpg?w=480&h=640&fit=crop&s=fcb556060c29623e706dfbc4eeca87ac',
+    'https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2',
+    'https://k72.ca/images/teamMembers/MEL_480X640.jpg?w=480&h=640&fit=crop&s=07c9bfee89816720b873e6748a276af6',
+    'https://k72.ca/images/teamMembers/CAMILLE_480X640_2.jpg?w=480&h=640&fit=crop&s=74317575b2d72fd11c5296615c383e4a',
+    'https://k72.ca/images/teamMembers/MEGGIE_480X640_2.jpg?w=480&h=640&fit=crop&s=3604b19f8fc7b40f517954147698d847',
+    'https://k72.ca/images/teamMembers/joel_480X640_3.jpg?w=480&h=640&fit=crop&s=1cadbf143b3aa916b1b414464acbb4d6',
+
+
+  ]
+
+  useGSAP(function () {
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        markers: true,
+        start: 'top 35%',
+        end: 'top -80%',
+        scrub: true,
+        pin: true,
+        onUpdate:(self)=>{
+          let ImageIndex;
+          if(self.progress<1){
+          ImageIndex=Math.round(self.progress * imagesArray.length)
+          }else{
+          ImageIndex=imagesArray.length -1
+          }
+         imageRef.current.src=imagesArray[ImageIndex]
+        }
+      }
+    })
+  })
   return (
-    <div>A</div>
+
+    <div>
+      <div className='section1'>
+        <div ref={imageDivRef} className='overflow-hidden w-[15vw] h-[20vw] rounded-4xl absolute top-96 left-[30vw]'>
+          <img ref={imageRef} className='h-full w-full' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
+        </div>
+        <div className='relative object-cover font-[font2] text-black'>
+          <div className=' mt-[55vh] '>
+            <h1 className=' text-[20vw] uppercase leading-[18vw] text-center  '>Soixan7e <br />
+              Douze</h1>
+          </div>
+          <div className='pl-[40%] mt-20'>
+            <p className='text-6xl '>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
+          </div>
+        </div>
+      </div>
+      <div className='section2 h-screen'>
+    
+      </div>
+    </div>
   )
 }
 
